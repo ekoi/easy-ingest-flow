cabb9093d4ab0550d42c7ef2179db560504982c9
Georgi Khomeriki <g.khomeriki@gmail.com>
2015-08-20 17:00:22 +0200

    step one (stage dataset) runs

--- src/main/scala/nl/knaw/dans/easy/ingest_flow/EasyIngestFlow.scala
+++ src/main/scala/nl/knaw/dans/easy/ingest_flow/EasyIngestFlow.scala
@@ -1,7 +1,26 @@
 package nl.knaw.dans.easy.ingest_flow
 
+import java.io.File
+
+import nl.knaw.dans.easy.stage.EasyStageDataset
+import org.slf4j.LoggerFactory
+
 object EasyIngestFlow {
+  val log = LoggerFactory.getLogger(getClass)
+
   def main(args: Array[String]) {
-    println("hello, friend")
+
+    implicit val stageSettings = nl.knaw.dans.easy.stage.Settings(
+      ownerId = "ownerId",
+      bagStorageLocation = "http://localhost/bags",
+      bagitDir = new File("test-resources/example-bag"),
+      sdoSetDir = new File("out/sdoSetDir"),
+      URN = "urn:nbn:nl:ui:13-1337-13",
+      DOI = "10.1000/xyz123")
+
+    for {
+      _ <- EasyStageDataset.run()
+    } yield log.info("hello, friend")
+
   }
 }
