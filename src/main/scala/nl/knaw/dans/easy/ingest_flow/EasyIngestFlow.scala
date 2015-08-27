44866e32e43caa238a052248f00f3e000d9a7abc
Georgi Khomeriki <g.khomeriki@gmail.com>
2015-08-27 17:08:56 +0200

    settings

--- src/main/scala/nl/knaw/dans/easy/ingest_flow/EasyIngestFlow.scala
+++ src/main/scala/nl/knaw/dans/easy/ingest_flow/EasyIngestFlow.scala
@@ -19,18 +19,44 @@ import scalaj.http.Http
 object EasyIngestFlow {
   val log = LoggerFactory.getLogger(getClass)
 
+  case class Settings(fedoraCredentials: FedoraCredentials,
+                      numSyncTries: Int,
+                      syncDelay: Long,
+                      ownerId: String,
+                      bagStorageLocation: String,
+                      bagitDir: String,
+                      sdoSetDir: String,
+                      DOI: String,
+                      postgresURL: String,
+                      solr: String,
+                      pidgen: String)
+
   def main(args: Array[String]) {
-    implicit val fedoraCreds = new FedoraCredentials(
-      "http://deasy:8080/fedora", "fedoraAdmin", "fedoraAdmin")
+
+    implicit val settings = Settings(
+      fedoraCredentials = new FedoraCredentials("http://deasy:8080/fedora", "fedoraAdmin", "fedoraAdmin"),
+      numSyncTries = 10,
+      syncDelay = 3000,
+      ownerId = "georgi",
+      bagStorageLocation = "http://localhost/bags",
+      bagitDir = "test-resources/example-bag",
+      sdoSetDir = "out/sdoSetDir",
+      DOI = "10.1000/xyz123",
+      postgresURL = "jdbc:postgresql://deasy:5432/easy_db?user=easy_webui&password=easy_webui",
+      solr = "http://deasy:8080/solr/datasets/update",
+      pidgen = "http://deasy:8082/pids?type=urn"
+    )
+
     run(new File("out/sdoSetDir")) match {
       case Success(datasetPid) => log.info(s"Finished, dataset pid: $datasetPid")
       case Failure(e) => throw e
     }
   }
 
-  def run(sdo: File)(implicit fedoraCreds: FedoraCredentials): Try[String] = {
+  def run(sdo: File)(implicit s: Settings): Try[String] = {
     for {
-      _ <- stageDataset(sdo)
+      urn <- requestURN()
+      _ <- stageDataset(sdo, urn)
       pidDictionary <- ingestDataset(sdo)
       datasetPid <- getDatasetPid(pidDictionary)
       _ <- waitForFedoraSync(datasetPid, pidDictionary, numTries = 10, delayMillis = 3000)
@@ -39,35 +65,35 @@ object EasyIngestFlow {
     } yield datasetPid
   }
 
-  def stageDataset(sdo: File): Try[Unit] = {
+  def stageDataset(sdo: File, urn: String)(implicit s: Settings): Try[Unit] = {
     log.info("Staging dataset")
     EasyStageDataset.run(stage.Settings(
-      ownerId = "georgi",
-      bagStorageLocation = "http://localhost/bags",
-      bagitDir = new File("test-resources/example-bag"),
+      ownerId = s.ownerId,
+      bagStorageLocation = s.bagStorageLocation,
+      bagitDir = new File(s.bagitDir),
       sdoSetDir = sdo,
-      URN = "urn:nbn:nl:ui:13-1337-13",
-      DOI = "10.1000/xyz123"))
+      URN = urn,
+      DOI = s.DOI))
   }
 
-  def ingestDataset(sdo: File)(implicit fedoraCreds: FedoraCredentials): Try[PidDictionary] = {
+  def ingestDataset(sdo: File)(implicit s: Settings): Try[PidDictionary] = {
     log.info("Ingesting staged digital object into Fedora")
-    EasyIngest.run(ingest.Settings(fedoraCreds, sdo))
+    EasyIngest.run(ingest.Settings(s.fedoraCredentials, sdo))
   }
 
-  def updateFsRdb(datasetPid: String)(implicit fedoraCreds: FedoraCredentials): Try[Unit] = {
+  def updateFsRdb(datasetPid: String)(implicit s: Settings): Try[Unit] = {
     log.info("Updating PostgreSQL database")
     FsRdbUpdater.run(fsrdb.Settings(
-      fedoraCredentials = fedoraCreds,
-      postgresURL = "jdbc:postgresql://deasy:5432/easy_db?user=easy_webui&password=easy_webui",
+      fedoraCredentials = s.fedoraCredentials,
+      postgresURL = s.postgresURL,
       datasetPid = datasetPid))
   }
 
-  def updateSolr(datasetPid: String)(implicit fedoraCreds: FedoraCredentials): Try[Unit] = {
+  def updateSolr(datasetPid: String)(implicit s: Settings): Try[Unit] = {
     log.info("Updating Solr index")
     EasyUpdateSolrIndex.run(solr.Settings(
-      fedoraCredentials = fedoraCreds,
-      solr = new URL("http://deasy:8080/solr/datasets/update"),
+      fedoraCredentials = s.fedoraCredentials,
+      solr = new URL(s.solr),
       dataset = datasetPid))
   }
 
@@ -78,7 +104,19 @@ object EasyIngestFlow {
     }
   }
 
-  def waitForFedoraSync(datasetPid: String, pidDictionary: PidDictionary, numTries: Int, delayMillis: Long)(implicit fedoraCreds: FedoraCredentials): Try[Unit] = {
+  def requestURN()(implicit s: Settings): Try[String] =
+    Try {
+      Http(s.pidgen)
+        .timeout(connTimeoutMs = 10000, readTimeoutMs = 50000)
+        .postForm.asString
+    }.flatMap(r =>
+      if (r.code == 200) {
+        val urn = r.body
+        log.info(s"Requested URN: $urn")
+        Success(urn)
+      } else Failure(new RuntimeException(s"PID Generator failed: ${r.body}")))
+
+  def waitForFedoraSync(datasetPid: String, pidDictionary: PidDictionary, numTries: Int, delayMillis: Long)(implicit s: Settings): Try[Unit] = {
     val expectedPids = pidDictionary.values.toSet - datasetPid
     @tailrec def loop(n: Int): Try[Unit] = {
       log.info(s"Check whether Fedora is synced. Tries left: $n.")
@@ -95,8 +133,8 @@ object EasyIngestFlow {
     loop(numTries)
   }
 
-  def queryPids(datasetPid: String)(implicit fedoraCreds: FedoraCredentials): Try[List[String]] = Try {
-    val url = s"${fedoraCreds.getBaseUrl}/risearch"
+  def queryPids(datasetPid: String)(implicit s: Settings): Try[List[String]] = Try {
+    val url = s"${s.fedoraCredentials.getBaseUrl}/risearch"
     val response = Http(url)
       .timeout(connTimeoutMs = 10000, readTimeoutMs = 50000)
       .param("type", "tuples")
